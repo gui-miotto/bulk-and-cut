61dd01c96645643c0d2fb99a672b9de5b13f651a
Gui Miotto <guilherme.miotto@gmail.com>
2020-08-19 15:48:00 +0200

    Merge bulkup_individual and slimdown_individual into generate_offspring

--- BulkAndCut/bulkandcut/evolution.py
+++ BulkAndCut/bulkandcut/evolution.py
@@ -36,7 +36,7 @@ class Evolution():
         self.debugging=debugging
 
         self.population = []
-        self.max_num_epochs = 3 #TODO: change to 50  # This is a project constraint
+        self.max_num_epochs = 2 #TODO: change to 50  # This is a project constraint
 
     def save_csv(self):
         file_path = os.path.join(self.work_directory, "population_summary.csv")
@@ -89,46 +89,19 @@ class Evolution():
             self.population.append(new_individual)
         self.save_csv()
 
-    def _slimdown_individual(self, parent_id):
-        parent_indv = self.population[parent_id]
-        parent_model = BNCmodel.LOAD(parent_indv.path_to_model)
-        child_model = parent_model.slimdown()
-        child_id = len(self.population)
-        path_to_child_model=self._get_model_path(indv_id=child_id)
-        performance = child_model.start_training(
-            n_epochs=self.max_num_epochs,
-            parent_model=parent_model,
-            train_data_loader=self.train_data_loader,
-            valid_data_loader=self.valid_data_loader,
-            train_fig_path=path_to_child_model + ".png"
-            )
-        child_id = len(self.population)
-        path_to_child_model=self._get_model_path(indv_id=child_id)
-        new_individual = Individual(
-            indv_id=child_id,
-            path_to_model=path_to_child_model,
-            summary=child_model.summary,
-            parent_id=parent_id,
-            bulk_counter=parent_indv.bulk_counter,
-            cut_counter=parent_indv.cut_counter + 1,
-            pre_training_loss=performance["pre_training_loss"],
-            post_training_loss=performance["post_training_loss"],
-            post_training_accuracy=performance["post_training_accuracy"],
-            n_parameters=child_model.n_parameters,
-        )
-        self.population.append(new_individual)
-        child_model.save(file_path=path_to_child_model)
-        new_individual.save_info()
-        self.save_csv()
+    def _generate_offspring(self, parent_id:int, transformation:str):
+        if transformation not in ["bulk-up", "slim-down"]:
+            raise Exception("Unknown transformation")
+        bulking = transformation == "bulk-up"
 
-    def _bulkup_individual(self, parent_id):
         parent_indv = self.population[parent_id]
         parent_model = BNCmodel.LOAD(parent_indv.path_to_model)
-        child_model = parent_model.bulkup()
+        child_model = parent_model.bulkup() if bulking else parent_model.slimdown()
         child_id = len(self.population)
         path_to_child_model=self._get_model_path(indv_id=child_id)
         performance = child_model.start_training(
             n_epochs=self.max_num_epochs,
+            parent_model=None if bulking else parent_model,
             train_data_loader=self.train_data_loader,
             valid_data_loader=self.valid_data_loader,
             train_fig_path=path_to_child_model + ".png" if self.debugging else None,
@@ -138,8 +111,8 @@ class Evolution():
             path_to_model=path_to_child_model,
             summary=child_model.summary,
             parent_id=parent_id,
-            bulk_counter=parent_indv.bulk_counter + 1,
-            cut_counter=parent_indv.cut_counter,
+            bulk_counter=parent_indv.bulk_counter + (1 if bulking else 0),
+            cut_counter=parent_indv.cut_counter + (0 if bulking else 1),
             pre_training_loss=performance["pre_training_loss"],
             post_training_loss=performance["post_training_loss"],
             post_training_accuracy=performance["post_training_accuracy"],
@@ -150,9 +123,15 @@ class Evolution():
         new_individual.save_info()
         self.save_csv()
 
-
-
     def run(self, time_budget:float):
+        self._create_work_directory()
+        self._train_initial_population()
+
+        self._generate_offspring(parent_id=len(self.population) - 1, transformation="bulk-up")
+        self._generate_offspring(parent_id=len(self.population) - 1, transformation="slim-down")
+
+
+    def run_(self, time_budget:float):
         run_start = datetime.now()
 
         self._create_work_directory()
@@ -160,18 +139,9 @@ class Evolution():
 
         # Check if we still have time:
         while (datetime.now() - run_start).seconds < time_budget:
-            self._bulkup_individual(parent_id=len(self.population) - 1)
-            self._bulkup_individual(parent_id=len(self.population) - 1)
-            self._slimdown_individual(parent_id=len(self.population) - 1)
-            self._slimdown_individual(parent_id=len(self.population) - 1)
-
+            self._generate_offspring(parent_id=len(self.population) - 1, transformation="bulk-up")
+            self._generate_offspring(parent_id=len(self.population) - 1, transformation="slim-down")
             break
-
-
-
-
-
-
 
 
 
