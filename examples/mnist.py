a43ff69a2c75a78e9d7dad0eaec7243217769aad
Gui Miotto <guilherme.miotto@gmail.com>
2020-09-14 13:27:21 +0200

    Update examples

--- examples/mnist.py
+++ examples/mnist.py
@@ -8,38 +8,27 @@ import bulkandcut as bnc
 
 here = os.path.dirname(__file__)
 data_dir = os.path.abspath(os.path.join(here, "..", "datasets", "mnist"))
-work_dir = os.path.join(here, "..", "..", "bulkandcut_output", str(datetime.now()))
+work_dir = os.path.join("/tmp", "bulkandcut_output", str(datetime.now()))
 
-batch_size = 256
 img_shape = (1, 28, 28)
 train_ds = torchvision.datasets.MNIST(
     root=data_dir,
     train=True,
     transform=torchvision.transforms.ToTensor(),
-    download=False,
+    download=True,
     )
 valid_ds = torchvision.datasets.MNIST(
     root=data_dir,
     train=False,
     transform=torchvision.transforms.ToTensor(),
-    download=False,
-    )
-train_loader = torch.utils.data.DataLoader(
-    dataset=train_ds,
-    batch_size=batch_size,
-    shuffle=True,
-    )
-valid_loader = torch.utils.data.DataLoader(
-    dataset=valid_ds,
-    batch_size=batch_size,
-    shuffle=False,
+    download=True,
     )
 evolution = bnc.Evolution(
     input_shape=img_shape,
     n_classes=len(train_ds.classes),
     work_directory=work_dir,
-    train_data_loader=train_loader,
-    valid_data_loader=valid_loader,
+    train_dataset=train_ds,
+    valid_dataset=valid_ds,
     debugging=True,
     )
-evolution.run(time_budget=3 * 60 * 60)
+evolution.run(time_budget=24. * 60. * 60.)
